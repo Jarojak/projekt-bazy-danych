e9927107951a8cf568a221e21b1174bd65b62c6e
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-04-03 11:10:19 -0400

    code formatting

--- index.php
+++ index.php
@@ -1,36 +1,36 @@
 <?php
 
 function getDefaultCategories($conn)
-    {
-        $sql = "SELECT *
-                FROM incomes_category_default";
+{
+    $sql = "SELECT *
+            FROM incomes_category_default";
 
-        $results = $conn->query($sql);
+    $results = $conn->query($sql);
 
-        return $results->fetchAll(PDO::FETCH_ASSOC);
-    }
+    return $results->fetchAll(PDO::FETCH_ASSOC);
+}
 
 function setDefaultCategoriesToUser($conn, $categories, $user_id)
-    {
-        $sql = "INSERT INTO incomes_category_assigned_to_users (user_id, name)
-                VALUES ";
+{
+    $sql = "INSERT INTO incomes_category_assigned_to_users (user_id, name)
+            VALUES ";
 
-        $values = [];
+    $values = [];
 
-        foreach ($categories as $id) {
-            $values[] = "($user_id, ?)";
-        }
-
-        $sql .= implode(", ", $values);
-
-        $stmt = $conn->prepare($sql);
-        
-        foreach ($categories as $i => $category) {
-            $stmt->bindValue($i + 1, $category["name"], PDO::PARAM_STR);
-        }
-
-        $stmt->execute();
+    foreach ($categories as $id) {
+        $values[] = "($user_id, ?)";
     }
+
+    $sql .= implode(", ", $values);
+
+    $stmt = $conn->prepare($sql);
+    
+    foreach ($categories as $i => $category) {
+        $stmt->bindValue($i + 1, $category["name"], PDO::PARAM_STR);
+    }
+
+    $stmt->execute();
+}
 
 function insertRandomIncomes($conn, $qty, $user_id)
 {
@@ -61,33 +61,33 @@ function insertRandomIncomes($conn, $qty, $user_id)
 }
 
 function getAmountByCategories($conn, $user_id, $date_begin, $date_end)
-    {
-        $sql = "SELECT 
-                    incomes_category_assigned_to_users.name AS category, 
-                    SUM(incomes.amount) AS amount 
-                FROM 
-                    incomes_category_assigned_to_users 
-                INNER JOIN 
-                    incomes ON 
-                    incomes.income_category_assigned_to_user_id 
-                    = incomes_category_assigned_to_users.id 
-                WHERE 
-                    incomes.user_id = :user_id
-                    AND 
-                    incomes.date_of_income BETWEEN :date_begin AND :date_end
-                GROUP BY incomes.income_category_assigned_to_user_id 
-                ORDER BY amount DESC;";
+{
+    $sql = "SELECT 
+                incomes_category_assigned_to_users.name AS category, 
+                SUM(incomes.amount) AS amount 
+            FROM 
+                incomes_category_assigned_to_users 
+            INNER JOIN 
+                incomes ON 
+                incomes.income_category_assigned_to_user_id 
+                = incomes_category_assigned_to_users.id 
+            WHERE 
+                incomes.user_id = :user_id
+                AND 
+                incomes.date_of_income BETWEEN :date_begin AND :date_end
+            GROUP BY incomes.income_category_assigned_to_user_id 
+            ORDER BY amount DESC;";
 
-        $stmt = $conn->prepare($sql);
+    $stmt = $conn->prepare($sql);
 
-        $stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
-        $stmt->bindValue(':date_begin', $date_begin, PDO::PARAM_STR);
-        $stmt->bindValue(':date_end', $date_end, PDO::PARAM_STR);
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
+    $stmt->bindValue(':date_begin', $date_begin, PDO::PARAM_STR);
+    $stmt->bindValue(':date_end', $date_end, PDO::PARAM_STR);
 
-        $stmt->execute();
+    $stmt->execute();
 
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
 require 'includes/init.php';
 
